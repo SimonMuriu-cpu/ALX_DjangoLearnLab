b61cbf1a54bca9bd53d64a92bc13f7225f438c01
Simon Muriu <muriusimon6@gmail.com>
2026-01-16 10:17:38 +0300

    Fixing query for specific author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,10 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+
 # 1️⃣ Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    return Book.objects.filter(author=author)
 
 
 # 2️⃣ List all books in a library
@@ -15,4 +16,4 @@ def get_books_in_library(library_name):
 # 3️⃣ Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
