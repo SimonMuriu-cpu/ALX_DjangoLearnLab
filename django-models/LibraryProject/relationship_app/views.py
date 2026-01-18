59939a783e7647b636b0203939d5726e74934835
Simon Muriu <muriusimon6@gmail.com>
2026-01-18 19:07:35 +0300

    Fixing import errors in relationship_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,7 +9,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
+
 from .forms import BookForm
 
 # Create your views here.
