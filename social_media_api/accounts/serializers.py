dd00c116eb9dfac05b47b4debf5a89a9048060a5
Simon Muriu <muriusimon6@gmail.com>
2026-02-22 10:44:30 +0300

    Fixing the RegisterSerializer in accounts/serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -14,14 +14,18 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         Token.objects.create(user=user)
         return user
-
+    
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
